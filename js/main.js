ab4e6cfb4a86d98a85ce52da75e4f57c6958bb74
Oleg Smyk <olegsmik25@gmail.com>
2019-04-08 21:07:05 +0300

    add portfolio modal

---
+++ js/main.js
@@ -0,0 +1,125 @@
+$(document).ready(function(){
+
+    // MODAL
+    var modalText = {
+      tajam: {
+        title: 'Roambi.com',
+        tag: 'web development.',
+        detail: 'Roambi provides analytics, reporting, and business intelligence for companies to use on the go. A Wordpress hosted site written in PHP and Javascript with Hubspot Integration.',
+      },
+      waxom: {
+        title: 'WalkerTracker',
+        tag: 'web development.',
+        detail: 'Walker Tracker offers goal management, fitness tracking, and team competitions to companies for internal use. A Ruby on Rails and Javascript companion site for the Walker Tracker App. Features visual metrics and gamified progression system.',
+      },
+      alexis: {
+        title: 'Powur.com',
+        tag: 'web development.',
+        detail: 'Powur is a multi-level marketing platform for lead generation, recruitment, and team building. Built with Ruby on Rails and Angular-UI. Makes use of Angular-material for front-end visuals. Features complex user tree heiarchy and commission system.',
+      },
+      startup: {
+        title: 'MyStand',
+        tag: 'web development.',
+        detail: 'MyStand is a crowd-funding, media sharing website, that has you donating actions instead of money out of your pocket. Single page App built with Node.js on Sails and Angular 2.0. Features social media sharing and large scale crowd-funding.',
+      },
+      new_providence: {
+        title: 'NeverSurrender',
+        tag: 'web development.',
+        detail: 'NeverSurrender is a platform for the new ALS foundation mobile app in hopes to raise awareness and research funding to fight ALS. Pure JavaScript marketing site to promote the new ALS NeverSurrender app.',
+      },
+      agnecy: {
+        title: 'The Mall',
+        tag: 'web development.',
+        detail: 'The Mall is a place to follow the latest fashion purchases of your friends and favorite celebrities. Built with Node.js and Handlebars. Features the ability to import thousands of top brands products into one shopping site.',
+      }
+    };
+  
+    $('.portfolio__item-btn').on('click', function(){
+      fillModal(this.id);
+      $('.modal-wrap').addClass('visible');
+    });
+  
+    $('.close').on('click', function(){
+      $('.modal-wrap, #modal .button').removeClass('visible');
+    });
+  
+    $('.mask').on('click', function(){
+      $('.modal-wrap, #modal .button').removeClass('visible');
+    });
+  
+    var carousel = $('#carousel'),
+        slideWidth = 700,
+        threshold = slideWidth/3,
+        dragStart, 
+        dragEnd;
+  
+    setDimensions();
+  
+    $('#next').click(function(){ shiftSlide(-1) })
+    $('#prev').click(function(){ shiftSlide(1) })
+  
+    carousel.on('mousedown', function(){
+      if (carousel.hasClass('transition')) return;
+      dragStart = event.pageX;
+      $(this).on('mousemove', function(){
+        dragEnd = event.pageX;
+        $(this).css('transform','translateX('+ dragPos() +'px)');
+      });
+      $(document).on('mouseup', function(){
+        if (dragPos() > threshold) { return shiftSlide(1) }
+        if (dragPos() < -threshold) { return shiftSlide(-1) }
+        shiftSlide(0);
+      });
+    });
+  
+    function setDimensions() {
+      if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+       slideWidth = $(window).innerWidth();
+      }
+      $('.carousel-wrap, .slide').css('width', slideWidth);
+      $('.modal').css('max-width', slideWidth);
+      $('#carousel').css('left', slideWidth * -1)
+    }
+  
+    function dragPos() {
+      return dragEnd - dragStart;
+    }
+  
+    function shiftSlide(direction) {
+      if (carousel.hasClass('transition')) return;
+      dragEnd = dragStart;
+      $(document).off('mouseup')
+      carousel.off('mousemove')
+              .addClass('transition')
+              .css('transform','translateX(' + (direction * slideWidth) + 'px)'); 
+      setTimeout(function(){
+        if (direction === 1) {
+          $('.slide:first').before($('.slide:last'));
+        } else if (direction === -1) {
+          $('.slide:last').after($('.slide:first'));
+        }
+        carousel.removeClass('transition')
+        carousel.css('transform','translateX(0px)'); 
+      },700)
+    }
+  
+    function fillModal(id) {
+      $('#modal .info__title').text(modalText[id].title);
+      $('#modal .info__detail').text(modalText[id].detail);
+      $('#modal .info__tag').text(modalText[id].tag);
+      if (modalText[id].link) $('#modal .button').addClass('visible')
+                                                 .parent()
+                                                 .attr('href', modalText[id].link)
+  
+      $.each($('#modal li'), function(index, value ) {
+        $(this).text(modalText[id].bullets[index]);
+      });
+      $.each($('#modal .slide'), function(index, value) {
+        $(this).css({
+          background: "url('img/slides/" + id + '-' + index + ".jpg') center center/cover",
+          backgroundSize: 'cover'
+        });
+                
+      });
+    }
+  })
