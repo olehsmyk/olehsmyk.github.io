65b2b6e218bf34ff84cad69400e42133e4ad73c2
Oleg Smyk <olegsmik25@gmail.com>
2019-04-10 12:18:30 +0300

    add toggle class in nav

--- js/main.js
+++ js/main.js
@@ -6,19 +6,41 @@ $(document).ready(function () {
     });
 
     $(window).on("scroll", function() {
-        var s = $(window).scrollTop();
-        var d = $(".main").innerHeight();
-        var main = $(".main__header").offset().top;
-        if(s <= d - 10) {
+        var window_h = $(window).scrollTop();
+        var main_h = $(".main").innerHeight();
+        var main__header = $(".main__header").offset().top;
+        if(window_h <= main_h - 10) {
             $("header")
                 .removeClass("header__scrolled")
-                .css("opacity",(main - s - 20)/main);
+                .css("opacity",(main__header - window_h - 20)/main__header);
         }
-        if(s > d - 10) {
+        if(window_h > main_h - 10) {
             $("header")
                 .addClass("header__scrolled")
                 .css("opacity", 1);
         }
+    });
+
+    function addNavActive(selector, number) {
+        var window_h = $(window).scrollTop();
+        var selector_h = $(selector).offset().top;
+        var header_h = $(".header").innerHeight();
+
+        if (($(window).innerHeight() + $(window).scrollTop()) >= $("body").height()) {
+            $('.header__nav-menu li').removeClass('active');
+            $(`.header__nav-menu li:nth-of-type(5)`).addClass('active');
+        }
+        else if(window_h + header_h >= selector_h) {
+            $('.header__nav-menu li').removeClass('active');
+            $(`.header__nav-menu li:nth-of-type(${number})`).addClass('active');
+        }
+    }
+    
+    $(window).on('scroll', function () {
+        addNavActive('#main', 1);
+        addNavActive('#about', 2);
+        addNavActive('#skills', 3);
+        addNavActive('#portfolio', 4);
     });
 });
 
