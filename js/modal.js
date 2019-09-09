7c58d4f0ed7c00247d338b4360a7668aad6ddf66
olegsmik25 <olegsmik25@gmail.com>
2019-09-09 13:52:32 +0300

    fix preventDefault error
    fix preventDefault error

--- js/modal.js
+++ js/modal.js
@@ -57,7 +57,7 @@ $(document).ready(function(){
     
     document.body.addEventListener('touchstart', function(e){
       if($('body').hasClass('block')) {
-        e.preventDefault();
+        // e.preventDefault();
       }
     });
   
